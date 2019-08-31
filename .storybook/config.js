44f137e9b07258d128a7858e3d104ca35d5fa01c
AlfieDarko <alfie.darko1@tesco.com>
2019-08-31 15:58:33 +0100

    taskbox UI

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
+
+import "../src/index.css";
+
+const req = requireContext("../src/components", true, /\.stories\.js$/);
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
