c8576ccbe8f6f221fae63def5f53eb53a3843e82
AlfieDarko <alfie.darko1@tesco.com>
2019-08-31 16:37:29 +0100

    commit before CI deploy

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask
+}) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
         <input
           type="checkbox"
-          defaultChecked={state === 'TASK_ARCHIVED'}
+          defaultChecked={state === "TASK_ARCHIVED"}
           disabled={true}
           name="checked"
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ textOverflow: "ellipsis" }}
+        />{" "}
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
-        {state !== 'TASK_ARCHIVED' && (
+        {state !== "TASK_ARCHIVED" && (
           <a href="/" onClick={() => onPinTask(id)}>
             <span className={`icon-star`} />
           </a>
@@ -33,8 +43,8 @@ Task.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
   }),
   onArchiveTask: PropTypes.func,
-  onPinTask: PropTypes.func,
+  onPinTask: PropTypes.func
 };
